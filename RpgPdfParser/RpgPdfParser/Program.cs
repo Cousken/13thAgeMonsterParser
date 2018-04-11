985308fbe62d3b6a6692749423961e39f113945c
Cousken <cousken@gmail.com>
2018-04-11 21:59:58 +0200

    First commit
    Able to extract text from PDF in a format which i think will allow me to parse monsters.

---
+++ RpgPdfParser/RpgPdfParser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace RpgPdfParser
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Contract.Assert(args.Length > 0 , "No arguments passed.");
+
+            var path = args.First();
+            var extractedText = string.Empty;
+            
+            using (var pdfReader = new PdfReader(path))
+            {
+                extractedText = PdfTextExtractor.GetTextFromPage(pdfReader, 208, new SimpleTextExtractionStrategy());
+            }
+
+            Console.WriteLine(extractedText);
+                
+            Console.WriteLine("END");
+            Console.ReadLine();
+        }
+    }
+    
+    
+}
