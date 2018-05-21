4c5e57f4d50a2b6946e013ee8172331b0e7c0105
Adam Politanski <cousken@gmail.com>
2018-05-21 13:14:37 +0200

    Just some work in progress i don't want to loose.

---
+++ RpgPdfParser/InstanceCompare.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace RpgPdfParser
+{
+    public struct Differance
+    {
+        public string Field;
+        public string LeftValue;
+        public string RightValue;
+    }
+
+    public static class InstanceCompare
+    {
+        public static List<Differance> Compare(object leftInstance, object rightInstance)
+        {
+            var leftType = leftInstance.GetType();
+            var rightType = leftInstance.GetType();
+
+            if (leftType != rightType)
+                throw new ArgumentException("Can't compare two instance of different types");
+
+            var result = new List<Differance>();
+
+            foreach (var property in leftType.GetProperties())
+            {
+                var leftValue = property.GetValue(leftInstance, null);
+                var rightValue = property.GetValue(rightInstance, null);
+                if (leftValue != rightValue)
+                    result.Add(new Differance()
+                    {
+                        Field = property.Name,
+                        LeftValue = leftValue.ToString(),
+                        RightValue = rightValue.ToString()
+                    });
+            }
+            
+            foreach (var field in leftType.GetFields())
+            {
+                var leftValue = field.GetValue(leftInstance);
+                var rightValue = field.GetValue(rightInstance);
+                if (leftValue != rightValue)
+                    result.Add(new Differance()
+                    {
+                        Field = field.Name,
+                        LeftValue = leftValue.ToString(),
+                        RightValue = rightValue.ToString()
+                    });
+            }
+
+            foreach (var method in leftType.GetMethods(BindingFlags.InvokeMethod | BindingFlags.Public))
+            {
+                if (method.GetGenericArguments().Any())
+                    continue;
+
+                try
+                {
+                    var leftValue = method.Invoke(leftInstance, null);
+                    var rightValue = method.Invoke(rightInstance, null);
+                    if (leftValue != rightValue)
+                        result.Add(new Differance()
+                        {
+                            Field = method.Name,
+                            LeftValue = leftValue.ToString(),
+                            RightValue = rightValue.ToString()
+                        });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- RpgPdfParser/Program.cs
+++ RpgPdfParser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Linq;
@@ -13,39 +14,52 @@ using iText.Layout.Element;
 
 namespace RpgPdfParser
 {
+    public struct StringWithFont
+    {
+        public string String;
+        public float Width;
+    }
+
     internal static class Program
     {
         private static TextToMonsterDataParser m_parser;
-        
+        public static List<StringWithFont> TextWithWidth = new List<StringWithFont>();
+
         public static void Main(string[] args)
         {
-            Contract.Assert(args.Any() , "No arguments passed.");
+            Contract.Assert(args.Any(), "No arguments passed.");
 
             var path = args.First();
             var extractedText = string.Empty;
-            
+
             using (var pdfReader = new PdfReader(path))
-            {                   
+            {
                 var pdfDocument = new PdfDocument(pdfReader);
-                var rect = new Rectangle(36, 750, 523, 56);
-                
-                var fontFilter = new CustomFilter(rect);
+                var page = pdfDocument.GetPage(208);
+
+                var fontFilter = new CustomFilter(page.GetArtBox());
                 var listener = new FilteredEventListener();
-                
-                var extractionStrategy = listener.AttachEventListener(new LocationTextExtractionStrategy(), fontFilter);
+
+                var extractionStrategy = listener.AttachEventListener(new SimpleTextExtractionStrategy(), fontFilter);
                 new PdfCanvasProcessor(listener).ProcessPageContent(pdfDocument.GetPage(208));
- 
+
                 extractedText = extractionStrategy.GetResultantText();
             }
-            
+
             m_parser = new TextToMonsterDataParser(extractedText);
             var monsters = m_parser.GetMonsterData();
 
             Console.WriteLine("END");
-            Console.ReadLine();
 
             Console.WriteLine(extractedText);
+            Console.ReadLine();
+            
+            foreach (var text in TextWithWidth)
+            {
+                Console.WriteLine("{0}({1})", text.String, text.Width);
+            }
 
+            
             Console.ReadLine();
         }
     }
@@ -54,35 +68,44 @@ namespace RpgPdfParser
     {
         public CustomFilter(Rectangle filterRect) : base(filterRect)
         {
-            
         }
 
         public override bool Accept(IEventData data, EventType type)
         {
-            if (!type.Equals(EventType.RENDER_TEXT)) 
+            if (!type.Equals(EventType.RENDER_TEXT))
                 return false;
-            
+
             var renderInfo = (TextRenderInfo) data;
             string text = renderInfo.GetText();
             if (text == null)
                 return false;
-            
-            if(text.Contains("Ant"))
-                Debugger.Break();
-            
-            if(text.Contains("ather-be-foraging-than-fighting"))
-                Debugger.Break();
-            
-            var font = renderInfo.GetFont();
-            var fontSize = renderInfo.GetFontSize();
-            
-            if (null == font) 
-                return false;
-            
-            
-            var fontName = font.GetFontProgram().GetFontNames().GetFontName();
-            return fontName.EndsWith("Bold") || fontName.EndsWith("Oblique");
+
+            var stringWithFont = new StringWithFont()
+            {
+                String = text,
+                Width = renderInfo.GetSingleSpaceWidth()
+            };
+
+            if (!Program.TextWithWidth.Any())
+            {
+                Program.TextWithWidth.Add(stringWithFont);
+                return true;
+            }
+                
+            var lastEntry = Program.TextWithWidth.Last();
+
+            if (Math.Abs(lastEntry.Width - stringWithFont.Width) < 0.001f)
+            {
+                Program.TextWithWidth.Remove(lastEntry);
+                lastEntry.String += stringWithFont.String;
+                Program.TextWithWidth.Add(lastEntry);
+            }
+            else
+            {
+                Program.TextWithWidth.Add(stringWithFont);
+            }
+
+            return true;
         }
     }
-    
 }
